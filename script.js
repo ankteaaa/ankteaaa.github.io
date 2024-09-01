61c83691146245a6d0cd555eca98eb0c5833f40b
ankteaaa <90596636+ankteaaa@users.noreply.github.com>
2024-09-01 22:03:14 +0200

    Burgermenu done
    Så er burgermenu og burgerikon virksomme med js. Very nice!
    
    Alle 3 sider (inkl. CV, som ikke har indhold og som ikke er publiceret) har fået burgerikon og burgermenu.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Mojn!");
+
+const menuicon = document.getElementById("menuicon");
+const burgermenu = document.getElementById("burgermenu");
+
+burgermenu.style.display = "none";
+burgermenu.style.color = "green";
+
+menuicon.onclick = openBurgermenu;
+menuicon.onmouseover = openBurgermenu;
+burgermenu.onclick = closeBurgermenu;
+
+function openBurgermenu(){
+    burgermenu.style.display = "block";
+}
+
+function closeBurgermenu(){
+    burgermenu.style.display = "none";
+}
